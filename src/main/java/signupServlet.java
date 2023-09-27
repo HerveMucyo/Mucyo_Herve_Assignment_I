4c075876fd3e40000a90e079721a8e5a73e24f05
Herve Mucyo <mucyoh32@gmail.com>
2023-09-27 14:24:59 +0200

    First commit

---
+++ src/main/java/signupServlet.java
@@ -0,0 +1,50 @@
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class signupServlet
+ */
+@WebServlet
+public class signupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		doGet(request, response);
+		// TODO Auto-generated method stub
+		String firstName = request.getParameter("first-name");
+		String lastName = request.getParameter("last-name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String confirmPassword = request.getParameter("confirm-password");
+		String birthdate = request.getParameter("birthdate");
+		String phone = request.getParameter("phone");
+		String address = request.getParameter("address");
+		String city = request.getParameter("city");
+		String country = request.getParameter("country");
+		
+		// Password validation
+		if (!password.equals(confirmPassword)) {
+		    // Passwords don't match, handle this error (e.g., show an error message)
+		    response.sendRedirect("signup.jsp?error=passwordMismatch");
+		    return;
+		}
+
+		// Email validation (simple check for illustration, you should use a more robust method)
+		if (!email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}")) {
+		    // Invalid email format, handle this error (e.g., show an error message)
+		    response.sendRedirect("signup.jsp?error=invalidEmail");
+		    return;
+		}
+		else {
+			// If everything is valid, you can save the user and redirect to a success page
+			response.sendRedirect("signup-success.jsp");
+
+		}
+	}
+
+}
---
+++ src/main/webapp/success.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Index</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+ <div class="container">
+        <h1>Login Successful</h1>
+        <i class="fas fa-thumbs-up fa-5x success-icon"></i>
+        <p>You have successfully logged in.</p>
+        <a href="Login.jsp" class="btn">Go Back</a>
+    </div>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
